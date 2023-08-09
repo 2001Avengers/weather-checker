61a92d2a21861c7fd17a7c2d8d2d3c778e72dad6
glyserin <serin.cheong@gmail.com>
2023-08-10 02:54:57 +0900

    Created project
    Created Spring Gradle project

---
+++ weather-checker/src/main/java/weatherchk/weatherchecker/WeatherCheckerApplication.java
@@ -0,0 +1,13 @@
+package weatherchk.weatherchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherCheckerApplication.class, args);
+	}
+
+}
