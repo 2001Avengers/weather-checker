ca8b51c2c146cc1d83fcb46262ec44facd0f61c0
glyserin <serin.cheong@gmail.com>
2023-08-12 19:59:58 +0900

    프로젝트 파일 생성
    Spring Inizializr로 Gradle 프로젝트 생성함

---
+++ weather-back/src/main/java/weather/weatherback/WeatherBackApplication.java
@@ -0,0 +1,13 @@
+package weather.weatherback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherBackApplication.class, args);
+	}
+
+}
---
+++ weather-back/src/test/java/weather/weatherback/WeatherBackApplicationTests.java
@@ -0,0 +1,13 @@
+package weather.weatherback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
